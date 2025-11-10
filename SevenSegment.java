d271c2132d96765ff43f0c543919d9a086dbe551
JPCass07 <JPCass07@outlook.com>
2025-11-10 05:01:46 +0000

    seven segment

---
+++ SevenSegment.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class SevenSegment {
+    // ASCII art for digits 0–9
+    private static String[][] DIGITS = {
+        { " _",
+          "| |",
+          "|_|" }, // 0
+        { "   ",
+          "  |",
+          "  |" }, // 1
+        { " _ ",
+          " _|",
+          "|_ " }, // 2
+        { " _ ",
+          " _|",
+          " _|" }, // 3
+        { "   ",
+          "|_|",
+          "  |" }, // 4
+        { " _ ",
+          "|_ ",
+          " _|" }, // 5
+        { " _ ",
+          "|_ ",
+          "|_|" }, // 6
+        { " _ ",
+          "  |",
+          "  |" }, // 7
+        { " _ ",
+          "|_|",
+          "|_|" }, // 8
+        { " _ ",
+          "|_|",
+          " _|" }  // 9
+    };
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a non-negative integer: ");
+        int number = in.nextInt();
+        in.close();
+
+        String numStr = Integer.toString(number);
+
+        // Build the three display rows
+        StringBuilder row1 = new StringBuilder();
+        StringBuilder row2 = new StringBuilder();
+        StringBuilder row3 = new StringBuilder();
+
+        for (char c : numStr.toCharArray()) {
+            int d = c - '0';
+            row1.append(DIGITS[d][0]).append(' ');
+            row2.append(DIGITS[d][1]).append(' ');
+            row3.append(DIGITS[d][2]).append(' ');
+        }
+
+        System.out.println(row1);
+        System.out.println(row2);
+        System.out.println(row3);
+    }
+}
